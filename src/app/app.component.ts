42192f48c7460be5d520c68c3a4d62114669f7c3
GwenMain <m_chudej@utb.cz>
2024-01-28 19:38:36 +0100

    added splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  showSplash = true;
+
+  constructor(private router: Router) {
+    setTimeout(() => {
+      this.showSplash = false;
+      this.router.navigate(['/home']); // Replace with your home route
+    }, 3000);
+  }
 }
+
